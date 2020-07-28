5eefb6b49dd99f431ed202f578d426e3bcefd39c
Sven65 <thormax10@gmail.com>
2020-07-28 16:35:31 +0200

    get item name base

---
+++ modules/API/src/main/java/xyz/mackan/ItemNames/ItemNames.java
@@ -0,0 +1,12 @@
+package xyz.mackan.ItemNames;
+
+import org.bukkit.inventory.ItemStack;
+
+public interface ItemNames {
+	/**
+	 * Gets the name of the ItemStack from server locale
+	 * @param itemStack ItemStack to get the item name from
+	 * @return String with the item name
+	 */
+	static String getItemName (ItemStack itemStack) { return ""; }
+}
