d1b7b3cfb91b74a7271a34bf791e067b4aada3b7
Sven65 <thormax10@gmail.com>
2020-08-13 09:59:49 +0200

    Add support for 1.16.2

---
+++ modules/1_16_R2/src/main/java/xyz/mackan/ItemNames/ItemNames_v1_16_R2.java
@@ -0,0 +1,18 @@
+package xyz.mackan.ItemNames;
+
+import net.minecraft.server.v1_16_R2.LocaleLanguage;
+import org.bukkit.craftbukkit.v1_16_R2.inventory.CraftItemStack;
+import org.bukkit.inventory.ItemStack;
+
+public class ItemNames_v1_16_R2 implements IItemNames {
+	/**
+	 * Gets the name of the ItemStack from server locale
+	 * @param itemStack ItemStack to get the item name from
+	 * @return String with the item name
+	 */
+	public String getItemName (ItemStack itemStack) {
+		net.minecraft.server.v1_16_R2.ItemStack nmsStack = CraftItemStack.asNMSCopy(itemStack);
+
+		return LocaleLanguage.a().a(nmsStack.getItem().getName());
+	}
+}
